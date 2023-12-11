40b0a2f1f2c19092cd9683f983c94f7d1191cc13
ngreenberg1 <112413778+ngreenberg1@users.noreply.github.com>
2023-12-11 13:57:20 -0600

    Create cifar_distribute_test.py

---
+++ cifar_distribute_test.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+strategy = tf.distribute.MirroredStrategy()
+print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
+
+cifar = tf.keras.datasets.cifar100
+
+new_input_shape = (128,128,3)
+
+(x_train, y_train), (x_test, y_test) = cifar.load_data()
+
+x_train_resized = tf.image.resize(x_train, new_input_shape[:2])
+x_test_resized = tf.image.resize(x_test, new_input_shape[:2])
+
+with strategy.scope():
+  model = tf.keras.applications.ResNet50(
+      include_top=True,
+      weights=None,
+      input_shape=new_input_shape,
+      classes=100,
+  )
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False)
+model.compile(optimizer="adam", loss=loss_fn, metrics=["accuracy"])
+            
+model.fit(x_train_resized, y_train, epochs=1, batch_size=32)
