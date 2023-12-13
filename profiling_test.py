68b06ec86b4aefb2a82b225637718c723ddde2c4
ngreenberg1 <112413778+ngreenberg1@users.noreply.github.com>
2023-12-13 14:14:18 -0600

    Update profiling_test.py

--- profiling_test.py
+++ profiling_test.py
@@ -13,9 +13,19 @@ new_input_shape = (224, 224, 3)
 # Load CIFAR-100 data
 (x_train, y_train), (x_test, y_test) = cifar.load_data()
 
+def resize_images(image, label):
+    image = tf.image.resize(image, new_input_shape[:2])
+    return image, label
+
+train_dataset = train_dataset.map(resize_images, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+train_dataset = train_dataset.shuffle(10000).batch(128).prefetch(tf.data.experimental.AUTOTUNE)
+
+test_dataset = test_dataset.map(resize_images, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+test_dataset = test_dataset.batch(128).prefetch(tf.data.experimental.AUTOTUNE)
+train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+
 # Resize images to the new input dimensions
-x_train_resized = tf.image.resize(x_train, new_input_shape[:2])
-x_test_resized = tf.image.resize(x_test, new_input_shape[:2])
 
 # Create the ResNet50 model with the new input shape
 model = tf.keras.applications.ResNet50(
@@ -32,4 +42,4 @@ model.compile(optimizer="adam", loss=loss_fn, metrics=["accuracy"])
 
 
 # Train the model
-model.fit(x_train_resized, y_train, epochs=1, batch_size=128, callbacks=[tensorboard_callback])
+model.fit(x_train_resized, y_train, epochs=1, callbacks=[tensorboard_callback])
